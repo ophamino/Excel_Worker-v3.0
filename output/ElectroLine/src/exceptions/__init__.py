3d664a8e4a4f4be251a49c2ae6001e0aad9da555
Alisultanov Dzhamaludin <alisultanovdzh@gmail.com>
2023-10-02 17:02:45 +0300

    delete output

--- output/ElectroLine/src/exceptions/__init__.py
+++
@@ -1,1 +0,0 @@
-from .bicu import BicuStatementNotFound
--- output/ElectroLine/src/exceptions/bicu.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-class FileStatementNotFound(Exception):
-    def __init__(self, *args: object) -> None:
-        if args:
-            logging.INFO(self.message)
-        else:
-            self.message = None
